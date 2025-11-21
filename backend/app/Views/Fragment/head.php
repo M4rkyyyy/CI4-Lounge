1dbf1a8464a2b8f4f01aaff586bb46990ba00782
M4rkyyyy <markyyyylicmoan@gmail.com>
2025-11-21 13:23:14 +0000

    fragment_main

--- backend/app/Views/Fragment/head.php
+++ backend/app/Views/Fragment/head.php
@@ -14,4 +14,3 @@
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
-<!--
---
+++ backend/app/Views/Fragment/header.php
@@ -0,0 +1,25 @@
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/logo no bg.png" alt="" style="width: 158px;">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                      <li><a href="index.php" class="active">Home</a></li>
+                      <li><a href="shop.php">Our Shop</a></li>
+                      
+                      <li><a href="checkout.php">Checkout</a></li>
+                      <li><a href="sign-in.php">Sign In</a></li>
+                  </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
