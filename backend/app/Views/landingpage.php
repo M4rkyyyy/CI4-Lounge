678586ed83d61e483e102ea6c709d6475214691f
Imot9239 <tpdl206@gmail.com>
2025-11-21 10:16:14 +0000

    update

---
+++ backend/app/Views/landingpage.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Lugx Gaming Shop HTML5 Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-lugx-gaming.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 589 lugx gaming
+
+https://templatemo.com/tm-589-lugx-gaming
+
+-->
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo no bg.png" alt="" style="width: 158px;">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                      <li><a href="index.html" class="active">Home</a></li>
+                      <li><a href="shop.html">Our Shop</a></li>
+                      
+                      <li><a href="checkout.html">Checkout</a></li>
+                      <li><a href="sign-in.html">Sign In</a></li>
+                  </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="main-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="caption header-text">
+            <h6>Welcome to Epic Quest Games</h6>
+            <h2>BEST RPG GENRE EVER!</h2>
+            <p>An RPG game lets you dive into epic adventures, shape your own story, and become the hero of a world full of endless possibilities! </p>
+            
+          </div>
+        </div>
+        <div class="col-lg-4 offset-lg-2">
+          <div class="right-image">
+            <img src="assets/images/the-witcher-3-best-rpgs-ezgif.com-webp-to-png-converter.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+
+  <div class="section trending">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="section-heading">
+            <h6>Trending</h6>
+            <h2>Trending Games</h2>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="main-button">
+            <a href="shop.html">View All</a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="skyrim-details.html"><img src="assets/images/trending-09.jpg" alt=""></a>
+              <span class="price"><em>₱1199</em> ₱299</span>
+            </div>
+            <div class="down-content">
+              <span class="category">MMORPG</span>
+              <h4>ELDER SCROLLS V</h4>
+              <a href="skyrim-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="genshin-details.html"><img src="assets/images/trending-06.jpg" alt=""></a>
+              <span class="price"><em>₱799</em> ₱299</span>
+            </div>
+            <div class="down-content">
+              <span class="category">OPEN-WORLD</span>
+              <h4>GENSHIN IMPACT</h4>
+              <a href="genshin-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="hsr-details.html"><img src="assets/images/hs.png" alt=""></a>
+              <span class="price"><em>₱899</em> ₱699</span>
+            </div>
+            <div class="down-content">
+              <span class="category">TURN-BASED</span>
+              <h4>HONKAI: STAR RAIL</h4>
+              <a href="hsr-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="wuwa-details.html"><img src="assets/images/wuthering.jpeg" alt=""></a>
+              <span class="price"><em>₱999</em> ₱499</span>
+            </div>
+            <div class="down-content">
+              <span class="category">OPEN-WORLD</span>
+              <h4>WUTHERING WAVES</h4>
+              <a href="wuwa-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section most-played">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="section-heading">
+            <h6>TOP GAMES</h6>
+            <h2>Most Played</h2>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="main-button">
+            <a href="shop.html">View All</a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="wuwa-details.html"><img src="assets/images/wuthering.jpeg" alt=""></a>
+            </div>
+            <div class="down-content">
+                <span class="category">OPEN-WORLD</span>
+                <h4>WUTHERING WAVES</h4>
+                <a href="wuwa-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="genshin-details.html"><img src="assets/images/trending-06.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+                <span class="category">OPEN-WORLD</span>
+                <h4>GENSHIN IMPACT</h4>
+                <a href="genshin-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="hsr-details.html"><img src="assets/images/hs.png" alt=""></a>
+            </div>
+            <div class="down-content">
+                <span class="category">TURN-BASED</span>
+                <h4>HONKAI: STAR RAIL</h4>
+                <a href="hsr-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="persona-details.html"><img src="assets/images/p.webp" alt=""></a>
+            </div>
+            <div class="down-content">
+                <span class="category">TURN-BASED</span>
+                <h4>PERSONA 5</h4>
+                <a href="persona-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="destiny-details.html"><img src="assets/images/trending-02.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+                <span class="category">MMORPG</span>
+                <h4>DESTINY 2</h4>
+                <a href="destiny-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="skyrim-details.html"><img src="assets/images/trending-09.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+                <span class="category">MMORPG</span>
+                <h4>ELDER SCROLLS V</h4>
+                <a href="skyrim-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section categories">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <div class="section-heading">
+            <h6>Categories</h6>
+            <h2>Top Categories</h2>
+          </div>
+        </div>
+        <div class="col-lg col-sm-6 col-xs-12">
+          <div class="item">
+            <h4>TURN-BASED</h4>
+            <div class="thumb">
+              <a href="shop.html?filter=turn-based"><img src="assets/images/Version_1.0_Wallpaper_star_rail.png" alt=""></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg col-sm-6 col-xs-12">
+          <div class="item">
+            <h4>MMORPG</h4>
+            <div class="thumb">
+              <a href="shop.html?filter=mmorpg"><img src="assets/images/final_fantasy.jpeg" alt=""></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg col-sm-6 col-xs-12">
+          <div class="item">
+            <h4>OPEN-WORLD</h4>
+            <div class="thumb">
+              <a href="shop.html?filter=open-world"><img src="assets/images/trending-06.jpg" alt=""></a>
+            </div>
+          </div>
+        </div>
+        
+        
+      </div>
+    </div>
+  </div>
+
+  
+  <div class="section cta">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="shop">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="section-heading">
+                  <h6>Our Shop</h6>
+                  <h2>Go Pre-Order Buy & Get Best <em>Prices</em> For You!</h2>
+                </div>
+                <p>Pre-order now and get exclusive freebies themed after your chosen game!</p>
+                <div class="main-button">
+                  <a href="shop.html">Shop Now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 offset-lg-2 align-self-end">
+          <div class="subscribe">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="section-heading">
+                  <h6>NEWSLETTER</h6>
+                  <h2>Get Up To ₱2,100 Off Just Buy <em>Subscribe</em> Newsletter!</h2>
+                </div>
+                <div class="search-input">
+                  <form id="subscribe" action="#">
+                    
+                    
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="col-lg-12">
+        <p>Copyright © 2025 Epic Quest Games. All rights reserved. &nbsp;&nbsp; <a rel="nofollow" href="https://templatemo.com" target="_blank">Design: TemplateMo</a></p>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/counter.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
